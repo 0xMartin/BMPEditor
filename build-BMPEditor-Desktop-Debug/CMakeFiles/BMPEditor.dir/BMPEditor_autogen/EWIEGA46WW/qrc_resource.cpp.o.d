52ca1f2774e2d014f9adf7c632c49b03a87d4ad4
Martin Krčma <58054719+0xMartin@users.noreply.github.com>
2025-06-01 13:03:22 +0200

    Delete build-BMPEditor-Desktop-Debug directory

--- build-BMPEditor-Desktop-Debug/CMakeFiles/BMPEditor.dir/BMPEditor_autogen/EWIEGA46WW/qrc_resource.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/BMPEditor.dir/BMPEditor_autogen/EWIEGA46WW/qrc_resource.cpp.o: \
- /home/martin/Downloads/BMPEditor/build-BMPEditor-Desktop-Debug/BMPEditor_autogen/EWIEGA46WW/qrc_resource.cpp \
- /usr/include/stdc-predef.h
